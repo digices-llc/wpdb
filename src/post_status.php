9ad263c4ffd2c16aad203a70a69c14669699e600
Roderic Linguri <linguri@digices.com>
2018-05-01 05:30:10 -0600

    Change PostStatus from Singleton

--- src/post_status.php
+++ src/post_status.php
@@ -3,9 +3,9 @@
 /**
   * @package   digices/wpdb
   * @module    post_status
-  * @author    Roderic Linguri <rlinguri@digices.com>
+  * @version   0.6.3
+  * @author    Roderic Linguri <linguri@digices.com>
   * @copyright Digices LLC
-  * @version   0.0.2
   */
 
 namespace digices\wpdb;
@@ -13,54 +13,57 @@ namespace digices\wpdb;
 class PostStatus
 {
 
-  /** @property PostMeta (instance) **/
-  protected static $shared;
+  /** @property integer **/
+  public $id;
+  
+  /** @property string **/
+  public $value;
 
-  protected $values;
-
-  /** @method shared (PostMeta getter) **/
-  public static function shared()
-  {
-    if (!isset(self::$shared)) {
-      self::$shared = new self();
-    }
-    return self::$shared;
-  } // ./shared
-
+  /** 
+    * @method  constructor
+    * @param   integer | string | null
+    * NOTE: passing null or nothing constructs a 'None' level
+    */
   public function __construct()
   {
-    $this->values = array('none','auto-draft','draft','failed','fue-active','fue-archived','fue-inactive','future','inherit','pending','preview','private','publish','trash','wc-active','wc-cancelled','wc-completed','wc-failed','wc-on-hold','wc-partial-payment','wc-pending','wc-pending-cancel','wc-pending-deposit','wc-refunded');
-  }
 
-  /**
-    * @method integer from string
-    * @param  string
-    * @return integer
-    */
-  public function integerFromString($str)
-  {
-    $i = 0;
-    foreach ($this->values as $value) {
-      if ($value == $str) {
-        return $i;
-      }
-      $i++;
-    }
-    return 0;
-  } // ./integerFromString
+    $values = array('none','auto-draft','draft','failed','fue-active','fue-archived','fue-inactive','future','inherit','pending','preview','private','publish','trash','wc-active','wc-cancelled','wc-completed','wc-failed','wc-on-hold','wc-partial-payment','wc-pending','wc-pending-cancel','wc-pending-deposit','wc-refunded');
 
-  /**
-    * @method integer from string
-    * @param  string
-    * @return integer
-    */
-  public function stringFromInteger($int)
-  {
-    if (isset($this->values[$int])) {
-      return $this->values[$int];
-    } else {
-      return 'none';
-    }
-  } // ./integerFromString
+    // create default values
+    $this->id = 0;
+    $this->value = 'none';
+    
+    // attempt to set from args
+    $args = func_get_args();
+    
+    if (count($args) > 0) {
+    
+      // only need the first arg
+      $arg = $args[0];
+
+      if (is_numeric($arg)) {
+        if (count($values) > $arg) {
+          $this->id = intval($arg);
+          $this->value = $values[$this->id];
+        } // ./we have enough values for the integer to make sense as an id
+      } // ./arg is numeric
+      
+      else {
+
+        $i = 0;
+
+        foreach ($values as $value) {
+          if ($value == $arg) {
+            $this->value = $arg;
+            $this->id = $i;
+          }
+          $i++;
+       } // ./foreach
+
+      } // ./arg is not numeric
+      
+    } // ./have args
+
+  } // ./__construct
 
 } // ./PostStatus
